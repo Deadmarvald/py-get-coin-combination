34c6572000dcc61a78e889aa170f23ebd7ea9b25
Deadmarvald <deadmarvald@gmail.com>
2024-09-24 21:24:45 +0300

    Fix flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -16,21 +16,24 @@ from app.main import get_coin_combination
             id="should convert 20 into 2 dimes"
         ),
         pytest.param(
-          5,
-          [0, 1, 0, 0],
-          id="should convert 5 into 1 nickels"
+            5,
+            [0, 1, 0, 0],
+            id="should convert 5 into 1 nickels"
         ),
         pytest.param(
-          4,
-          [4, 0, 0, 0],
-          id="should convert 4 into 4 pennies"
+            4,
+            [4, 0, 0, 0],
+            id="should convert 4 into 4 pennies"
         ),
         pytest.param(
-          41,
-          [1, 1, 1, 1],
-          id="should convert 41 into all coin types"
+            41,
+            [1, 1, 1, 1],
+            id="should convert 41 into all coin types"
         ),
     ]
 )
-def test_get_coin_combination(amount_of_cents: int, expected_result: list) -> None:
+def test_get_coin_combination(
+        amount_of_cents: int,
+        expected_result: list
+) -> None:
     assert get_coin_combination(amount_of_cents) == expected_result
